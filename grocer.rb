136ba3f56e3d7a3d96ddf0868cccde15492a13e1
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-24 12:09:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,6 +21,7 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]
+<<<<<<< HEAD
     coupon_item = "#{item} W/COUPON"
     if cart.has_key?(item)
       if !cart[coupon_item] && cart[item][:count] >= coupon[:num]
@@ -28,6 +29,14 @@ def apply_coupons(cart, coupons)
         cart[item][:count] -= coupon[:num]
       elsif cart[coupon_item] && cart[item][:count] >= coupon[:num]
         cart[coupon_item][:count] += coupon[:num]
+=======
+    if cart.has_key?(item)
+      if !cart["#{item} W/COUPON"] && cart[item][:count] >= coupon[:num]
+        cart["#{item} W/COUPON"] = {count: coupon[:num], price: coupon[:cost]/coupon[:num], clearance: cart[item][:clearance]}
+        cart[item][:count] -= coupon[:num]
+      elsif cart["#{item} W/ COUPON"] && cart[item][:count] >= coupon[:num]
+        cart["#{item} W/COUPON"][:count] += coupon[:num]
+>>>>>>> 20f61d8e5b1a5c7758e8599c02f31cd118a6657d
         cart[item][:count] -= coupon[:num]
       end
     end
@@ -45,7 +54,11 @@ end
 def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
+<<<<<<< HEAD
   applied_discount = apply_clearance(applied_coupons)
+=======
+  applied_clearance = apply_clearance(applied_coupons)
+>>>>>>> 20f61d8e5b1a5c7758e8599c02f31cd118a6657d
   total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
   total > 100 ? total * 0.9 : total 
 end
@@ -53,3 +66,13 @@ end
 
 
 
+<<<<<<< HEAD
+=======
+
+
+
+
+
+
+
+>>>>>>> 20f61d8e5b1a5c7758e8599c02f31cd118a6657d
